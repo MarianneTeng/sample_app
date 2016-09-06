3e57af171200dd6d7b8b868f336f2a22f6335d6c
Marianne Teng <mteng@digidentity.com>
2016-09-06 14:14:50 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  root 'application#...'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
